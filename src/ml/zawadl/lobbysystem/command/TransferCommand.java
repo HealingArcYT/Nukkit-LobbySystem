1d316477ab988724c6ee2e320be277cf4defe4cb
Daniel <molld807@gmail.com>
2021-03-11 14:56:53 +0100

    Several changes to /server

--- src/ml/zawadl/lobbysystem/command/TransferCommand.java
+++ src/ml/zawadl/lobbysystem/command/TransferCommand.java
@@ -3,13 +3,15 @@ package ml.zawadl.lobbysystem.command;
 import cn.nukkit.Player;
 import cn.nukkit.command.CommandSender;
 import cn.nukkit.command.PluginCommand;
+
 import ml.zawadl.lobbysystem.LobbySystem;
+
 
 public class TransferCommand extends PluginCommand<LobbySystem> {
     private final LobbySystem plugin;
 
     public TransferCommand(LobbySystem plugin) {
-        super("/server", plugin);
+        super("server", plugin);
         this.setPermission("lobbysystem.server");
         this.plugin = plugin;
     }
